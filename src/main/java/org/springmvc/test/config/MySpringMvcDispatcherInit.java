d7d6e979d22cb23732224b355c366b66b7a78ccf
Sasha <laplata228@gmail.com>
2023-07-30 19:39:59 +0300

    @Controller

---
+++ src/main/java/org/springmvc/test/config/MySpringMvcDispatcherInit.java
@@ -0,0 +1,20 @@
+package org.springmvc.test.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
